455ead1202125bfe526573b67f4e2796d2f33180
zakuni <kunio038@gmail.com>
2012-10-29 21:06:57 +0900

    updated machiawase and show near_station

--- main.rb
+++ main.rb
@@ -20,8 +20,12 @@ class App < Sinatra::Base
 
   post '/' do
     "#{params[:text]} #{params[:text2]}"
-    m = Machiawase.new
-    @j = JSON.parser.new(m.middle_of(params[:text], params[:text2])).parse()
+    g1 = Machiawase::Place.geocode(params[:text])
+    p1 = Machiawase::Place.new(g1["lat"], g1["lon"])
+    g2 = Machiawase::Place.geocode(params[:text2])
+    p2 = Machiawase::Place.new(g2["lat"], g2["lon"])
+    m = Machiawase.new(p1, p2)
+    @j = JSON.parse(m.to_json)
     slim :result
   end
 end
